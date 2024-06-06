36d509c8704d9f57936a294aeea7426d681cdba9
Victor-Ortiz-Ruiz <voruiz@gmail.com>
2024-06-06 16:05:55 -0600

    Change applied as testing

--- js/mapaSismos.js
+++ js/mapaSismos.js
@@ -23,7 +23,7 @@ export function initMapaSismos() {
             .attr("fill", "#ccc")
             .attr("stroke", "#333");
 
-        d3.csv("data/sismos.csv").then(sismos => {
+        /* d3.csv("data/sismos.csv").then(sismos => {
             const tooltip = createTooltip();
 
             svgMapa.selectAll("circle")
@@ -40,7 +40,78 @@ export function initMapaSismos() {
                 })
                 .on("mouseout", function(d) {
                     hideTooltip(tooltip);
-                });
+                }); */
+
+        let size = d3.scaleSqrt()
+            .domain(valueExtent) // Que hay en los datos?
+            .rang([1, 20]) //tamaño del circulo en pixeles
+
+        d3.csv("data/sismos_1975-2024.csv", (sismos) => {
+
+        svgMapa
+            .selectAll("CR_Sismos")
+            .data(sismos.filter((d) => { return d.year == "1975" }))
+            .enter()
+            .append("circle")
+            .attr("cx", d => projection([+d.longitude, +d.latitude])[0] )
+            .attr("cy", d => projection([+d.longitude, +d.latitude])[1] )
+            .attr("r",  d => size(d.magnitude + 1) + 2 )
+            .style("fill", "red" /* (d) => { return color(d.month) } */)
+            /* .attr("stroke-width", 1) */
+            /* .attr("fill-opacity", 0.6) */
+            .attr("opacity", 0.6)
+            .on("mouseover", () => { return tooltip.style("visibility", "visible"); })
+            .on("mousemove", (d) => { 
+                tooltip.text(d.year + ' (' + d.month + ' Mes)');
+                   return tooltip.style("top", 
+                (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
         });
-    });
+     })
+ });
+
+/*     //Datos de los sismos
+    d3.csv("data/sismos_1975-2022.csv", (sismos) => {
+
+        //Escala de colores
+        let color = d3.scale.category20();
+
+        //Se agrega una escala para el tamaño de la burbuja
+        let valueExtent = d3.extent(sismos, (d) => { return d.magnitude; })
+        let size = d3.scaleSqrt()
+            .domain(valueExtent) // Que hay en los datos?
+            .rang([1, 20]) //tamaño del circulo en pixeles
+
+        let tooltip = d3.select("body")
+            .append("div")
+            .style("position", "absolute")
+            .style("text-align", "center")
+            .style("padding", "15px")
+            .style("font", "12px sans-serif")
+            .style("background", "#03bafc")
+            .style("border", "0px")
+            .style("border-radius", "8px")
+            .style("z-index", "10")
+            .style("visibility", "hidden")
+            .style("a simple tooltip"); */
+
+/*         svgMapa
+            .selectAll("CR_Sismos")
+            .data(sismos.filter((d) => { return d.year == "1975" })) */
+            /* .data(data.sort((a, b) => { return +b.magnitude - +a.magnitude }).filter((d, i) => { return i < 1000 })) */
+/*             .enter()
+            .append("circle")
+            .attr("cx", (d) => { return projection([+d.longitude, +d.latitude])[0] })
+            .attr("cy", (d) => { return projection([+d.longitude, +d.latitude])[1] })
+            .attr("r",  (d) => { return size(d.magnitude + 1) + 2 })
+            .style("fill", "red" /* (d) => { return color(d.month) } ) */
+            /* .attr("stroke-width", 1) */
+            /* .attr("fill-opacity", 0.6) */
+/*             .attr("opacity", 0.6)
+            .on("mouseover", () => { return tooltip.style("visibility", "visible"); })
+            .on("mousemove", (d) => { 
+                tooltip.text(d.year + ' (' + d.month + ' Mes)');
+                return tooltip.style("top", 
+                    (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
+            }); */
+    /* });  */
 }
